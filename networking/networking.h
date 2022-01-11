a5dd55570028293f14ed22ebb92fb98f37b8edca
JonAlf <jonalf@gmail.com>
2022-01-11 09:47:08 -0500

    fized networking.h:

--- networking/networking.h
+++ networking/networking.h
@@ -1,175 +1,30 @@
-#include "networking.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
 
-void error_check( int i, char *s ) {
-  if ( i < 0 ) {
-    printf("[%s] error %d: %s\n", s, errno, strerror(errno) );
-    exit(1);
-  }
-}
+#ifndef NETWORKING_H
+#define NETWORKING_H
 
-/*=========================
-  server_setup
-  args:
-
-  creates, binds a server side socket
-  and sets it to the listening state
-
-  returns the socket descriptor
-  =========================*/
-int server_setup() {
-  int sd, i;
-
-  //setup structs for getaddrinfo
-  struct addrinfo * hints, * results;
-  hints = calloc(1, sizeof(struct addrinfo));
-  hints->ai_family = AF_INET6;  //IPv4 address
-  hints->ai_socktype = SOCK_STREAM;  //TCP socket
-  hints->ai_flags = AI_PASSIVE;  //Use all valid addresses
-  getaddrinfo(NULL, PORT, hints, &results); //NULL means use local address
-
-  //create the socket
-  sd = socket( results->ai_family, results->ai_socktype, results->ai_protocol );
-  error_check( sd, "server socket" );
-  printf("[server] socket created\n");
+#define BUFFER_SIZE 256
+#define PORT "9001"
+#define TEST_IP "127.0.0.1"
 
 
-  //bind the socket to address and port
-  i = bind( sd, results->ai_addr, results->ai_addrlen );
-  error_check( i, "server bind" );
-  printf("[server] socket bound\n");
+void error_check( int i, char *s );
 
-  //set socket to listen state
-  i = listen(sd, 10);
-  error_check( i, "server listen" );
-  printf("[server] socket in listen state\n");
+int server_setup();
+int server_connect(int sd);
 
-  //this code should get around the address in use error
-  int yes = 1;
-  if ( setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1 ) {
-    printf("sockopt  error\n");
-    exit(-1);
-  }
+int client_setup(char * server);
 
-  //free the structs used by getaddrinfo
-  free(hints);
-  freeaddrinfo(results);
-  return sd;
-}
+int udp_server_setup();
+struct addrinfo *  udp_client_setup(char * server, int *sock);
 
-
-/*=========================
-  server_connect
-  args: int sd
-
-  sd should refer to a socket in the listening state
-  run the accept call
-
-  returns the socket descriptor for the new socket connected
-  to the client.
-  =========================*/
-int server_connect(int sd) {
-  int client_socket;
-  socklen_t sock_size;
-  struct sockaddr_storage client_address;
-
-  sock_size = sizeof(client_address);
-  client_socket = accept(sd, (struct sockaddr *)&client_address, &sock_size);
-  error_check(client_socket, "server accept");
-
-  return client_socket;
-}
-
-/*=========================
-  client_setup
-  args: int * to_server
-
-  to_server is a string representing the server address
-
-  create and connect a socket to a server socket that is
-  in the listening state
-
-  returns the file descriptor for the socket
-  =========================*/
-int client_setup(char * server) {
-  int sd, i;
-
-  //run getaddrinfo
-  /* hints->ai_flags not needed because the client
-     specifies the desired address. */
-  struct addrinfo * hints, * results;
-  hints = (struct addrinfo *)calloc(1, sizeof(struct addrinfo));
-  hints->ai_family = AF_INET6;  //IPv4
-  hints->ai_socktype = SOCK_STREAM;  //TCP socket
-  getaddrinfo(server, PORT, hints, &results);
-
-  //create the socket
-  sd = socket( results->ai_family, results->ai_socktype, results->ai_protocol );
-  error_check( sd, "client socket" );
-
-  //connect to the server
-  //connect will bind the socket for us
-  i = connect( sd, results->ai_addr, results->ai_addrlen );
-  error_check( i, "client connect" );
-
-  free(hints);
-  freeaddrinfo(results);
-
-  return sd;
-}
-
-int udp_server_setup() {
-  int sd, i;
-
-  //setup structs for getaddrinfo
-  struct addrinfo * hints, * results;
-  hints = calloc(1, sizeof(struct addrinfo));
-  hints->ai_family = AF_INET6;  //IPv4 address
-  hints->ai_socktype = SOCK_DGRAM;  //UDP socket
-  hints->ai_flags = AI_PASSIVE;  //Use all valid addresses
-  getaddrinfo(NULL, PORT, hints, &results); //NULL means use local address
-
-  //create the socket
-  sd = socket( results->ai_family, results->ai_socktype, results->ai_protocol );
-  error_check( sd, "server socket" );
-  printf("[server] socket created\n");
-
-
-  //bind the socket to address and port
-  i = bind( sd, results->ai_addr, results->ai_addrlen );
-  error_check( i, "server bind" );
-  printf("[server] socket bound\n");
-
-  //this code should get around the address in use error
-  int yes = 1;
-  if ( setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1 ) {
-    printf("sockopt  error\n");
-    exit(-1);
-  }
-
-  //free the structs used by getaddrinfo
-  free(hints);
-  freeaddrinfo(results);
-  return sd;
-}
-
-
-
-struct addrinfo *  udp_client_setup(char * server, int *sock) {
-
-  //run getaddrinfo
-  /* hints->ai_flags not needed because the client
-     specifies the desired address. */
-  struct addrinfo * hints, * results;
-  hints = (struct addrinfo *)calloc(1, sizeof(struct addrinfo));
-  hints->ai_family = AF_INET6;  //IPv4
-  hints->ai_socktype = SOCK_DGRAM;  //TCP socket
-  getaddrinfo(server, PORT, hints, &results);
-
-  //create the socket
-  *sock = socket( results->ai_family, results->ai_socktype, results->ai_protocol );
-  error_check( *sock, "client socket" );
-
-  free(hints);
-
-  return results;
-}
+#endif
